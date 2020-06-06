208868bb6dd97db3245025727fbd2aab67233f3b
Clint Desktop <cdecker18@gmail.com>
2020-06-05 21:07:03 -0700

    Continuing to add to the Visualization

---
+++ Visualization/Helper/createFigure.py
@@ -0,0 +1,111 @@
+import pandas as pd
+
+import plotly.graph_objects as go
+
+"""
+    Returns initial data for one frame
+"""
+def getInitialData(pandasFrame):
+    xArray = pandasFrame['x']
+    yArray = pandasFrame['y']
+    data = data=[go.Scatter(
+                x = [xArray[0]],
+                y = [yArray[0]],
+                mode="markers",
+                marker={
+                    "sizemode": "area",
+                    "sizeref": 200000,
+                    "size": 1
+                },
+                name = "tag"
+            )]
+
+    return data
+
+"""
+    Returns frames for the animation
+"""
+def getFrames(pandasFrame, increment):
+    list_of_frames = []
+
+    xArray = pandasFrame['x']
+    yArray = pandasFrame['y']
+
+    for k in range(xArray.size):
+        list_of_frames.append(
+            go.Frame(
+                data=[go.Scatter(
+                    x = [xArray[k * increment]],
+                    y = [yArray[k * increment]],
+                    mode="markers",
+                    marker={
+                        "size": 6,
+                        "line": {
+                            "width":2, 
+                            "color": 'DarkSlateGrey'
+                        }
+                    },
+                    name = "tag"
+                )]
+            )
+        )
+
+
+    return list_of_frames
+
+def getAllInitialData(pandasFrame, length):
+    data = []
+    for k in range(length):
+        xArray = pandasFrame[k]['x']
+        yArray = pandasFrame[k]['y']
+        data.append(go.Scatter(
+            x = [xArray[0]],
+            y = [yArray[0]],
+            mode="markers",
+            marker={
+                "size": 6,
+                    "line": {
+                    "width":2, 
+                    "color": 'DarkSlateGrey'
+                }
+            },
+            name = "tag" + str(k)
+        ))
+
+    return data
+
+def getCurrentData(allFrames, idx):
+    data = []
+    for k in range(len(allFrames)):
+        xArray = allFrames[k]['x']
+        yArray = allFrames[k]['y']
+        data.append(
+            go.Scatter(
+                x = [xArray[idx]],
+                y = [yArray[idx]],
+                mode="markers",
+                marker={
+                    "size": 6,
+                    "line": {
+                        "width":2, 
+                        "color": 'DarkSlateGrey'
+                    }
+                },
+                name = "tag" + str(k)
+            )
+        )
+    
+    return data
+
+def getAllFrames(allFrames, increment, length):
+    list_of_frames = []
+
+    for k in range(length):
+        list_of_frames.append(
+            go.Frame(
+                data=getCurrentData(allFrames, k * increment)
+            )
+        )
+
+
+    return list_of_frames
--- Visualization/app.py
+++ Visualization/app.py
@@ -8,10 +8,14 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 from plotly.figure_factory import create_2d_density
+from Helper.createFigure import getInitialData, getFrames, getAllInitialData, getAllFrames
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+xMinMax = 27
+yMinMax = 69
 
 colors = {
     'background': '#111111',
@@ -27,24 +31,64 @@ for tag in range(tagCount):
     allFiles.append("csvs/tag{0}.csv".format(tag+1))
 
 li = []
+li2 = []
 
 for filename in allFiles:
     df = pd.read_csv(filename, index_col=None, header=0)
     li.append(df)
 
+for dframe in li:
+    df = dframe.loc[(dframe['time'] * 1.0) % 60 == 0]
+    li2.append(df)
+
 frame = pd.concat(li, axis=0, ignore_index=True)
 
 df = frame.loc[(frame['time'] * 1.0) % 60 == 0]
+
+df2 = li[0].loc[(frame['time'] * 1.0) % 60 == 0]
 
 fig = px.scatter(frame, x="x", y="y", color="tag", title="Scatter attempt")
 
 fig2 = create_2d_density(
     frame.x, frame.y, colorscale=colorscale,
     hist_color='rgb(255, 237, 222)', point_size=1, title='2D Density Plot', 
-    height=600, width=600
+    height=600, width=1200
 )
 
-fig3 = px.scatter(df, x="x", y="y", animation_frame="time", animation_group="tag", color="tag", hover_name="tag", size="time", size_max=1)
+#fig3 = px.scatter(df, x="x", y="y", animation_frame="time", animation_group="tag", color="tag", hover_name="tag", size="time", size_max=1)
+fig3 = go.Figure(
+    data=getInitialData(df2),
+    layout=go.Layout(
+        xaxis=dict(range=[-xMinMax, xMinMax], autorange=False),
+        yaxis=dict(range=[-yMinMax, yMinMax], autorange=False),
+        title="Tag 1 Tracker",
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          args=[None])])],
+        height=1600,
+        width=1200
+    ),
+    frames=getFrames(df2, 12)
+)
+
+fig4 = go.Figure(
+    data=getAllInitialData(li2, len(li2)),
+    layout=go.Layout(
+        xaxis=dict(range=[-xMinMax, xMinMax], autorange=False),
+        yaxis=dict(range=[-yMinMax, yMinMax], autorange=False),
+        title="All Tag Tracker",
+        updatemenus=[dict(
+            type="buttons",
+            buttons=[dict(label="Play",
+                          method="animate",
+                          args=[None])])],
+        height=1600,
+        width=1200
+    ),
+    frames=getAllFrames(li2, 12, li2[0]['x'].size)
+)
 
 app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
     html.H1(
@@ -56,16 +100,19 @@ app.layout = html.Div(style={'backgroundColor': colors['background']}, children=
     ),
 
     html.Div(children=['Tracking of the movements of the cows',
-        dcc.Graph(figure=fig),
+            dcc.Graph(figure=fig)
+        ],
 
-        dcc.Graph(figure=fig2),
-
-        dcc.Graph(figure=fig3)],
-        
         style={
         'textAlign': 'center',
         'color': colors['text']
-    })
+    }),
+
+    dcc.Graph(figure=fig2),
+
+    dcc.Graph(figure=fig3),
+
+    dcc.Graph(figure=fig4)
 ])
 
 
